fff29f3e6ff0af7b639e793496048113477d51ac
James Crook <james.k.crook@gmail.com>
2015-04-18 18:29:56 +0100

    Makefile.am updates.

---
+++ src/ViewInfo.cpp
@@ -0,0 +1,25 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ViewInfo.cpp
+
+  Paul Licameli
+
+*******************************************************************//*!
+
+\class ViewInfo
+\brief This class does something.
+
+*//*******************************************************************/
+
+#include "Audacity.h"
+
+#include <wx/defs.h>
+#include "Diags.h"
+#include "ViewInfo.h"
+
+void DummyFunction()
+{
+   DIAG("The Dummy function in ViewInfo was called" );
+}
