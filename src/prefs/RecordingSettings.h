14e381ca4b5c9a993ad830d8fefb2d83ec70a18b
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-04 14:50:59 -0500

    Files holding settings separated from definitions of panels

---
+++ src/prefs/RecordingSettings.h
@@ -0,0 +1,14 @@
+/**********************************************************************
+ 
+ Audacity: A Digital Audio Editor
+ 
+ RecordingSettings.h
+ 
+ Paul Licameli split from RecordingPrefs.h
+ 
+ **********************************************************************/
+
+#ifndef __AUDACITY_RECORDING_SETTINGS__
+#define __AUDACITY_RECORDING_SETTINGS__
+
+#endif
