1abf05bc65aad90efb5aa5b8b853f9e3f77382fb
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-28 10:56:53 -0500

    Move sampleCount to new files

---
+++ src/SampleTrack.cpp
@@ -0,0 +1,44 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+SampleTrack.cpp
+
+Paul Licameli split from WaveTrack.cpp
+
+**********************************************************************/
+
+#include "SampleTrack.h"
+#include "SampleCount.h"
+
+SampleTrack::~SampleTrack() = default;
+
+static const Track::TypeInfo &typeInfo()
+{
+   static const Track::TypeInfo info{
+      { "sample", "sample", XO("Sample Track") },
+      true, &PlayableTrack::ClassTypeInfo() };
+   return info;
+}
+
+static TrackTypeRegistry::RegisteredType sType{ "Sample", typeInfo() };
+
+auto SampleTrack::ClassTypeInfo() -> const TypeInfo &
+{
+   return typeInfo();
+}
+
+auto SampleTrack::GetTypeInfo() const -> const TypeInfo &
+{
+   return typeInfo();
+}
+
+sampleCount SampleTrack::TimeToLongSamples(double t0) const
+{
+   return sampleCount( floor(t0 * GetRate() + 0.5) );
+}
+
+double SampleTrack::LongSamplesToTime(sampleCount pos) const
+{
+   return pos.as_double() / GetRate();
+}
